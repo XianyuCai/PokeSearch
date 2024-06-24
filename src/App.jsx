ffcae2c9ec649a6029266f2c5dc41b141a040f90
Cai <caixianyu8@gmail.com>
2024-06-24 15:22:37 +1000

    Add search functionality using Enter key

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,61 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
+  // state variables
   const [currentValue, setCurrentValue] = useState("");
   const [searchValue, setSearchValue] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
   const [imageURL, setImageURL] = useState("");
 
+  // fetch pokemon data when searchValue change
   useEffect(() => {
     if (searchValue !== "") {
-      fetch("https://pokeapi.co/api/v2/pokemon/" + searchValue)
-      .then((data) => {
-        return data.json()
-      })
-      .then((json_data) => {
-        console.log(json_data)
-        setImageURL(json_data.sprites.front_default);
-        setIsLoaded(true)
-      })
+      fetchPokemonImage(searchValue);
     }
-  }, [searchValue])
+  }, [searchValue]);
+
+  const fetchPokemonImage = (pokemonName) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setImageURL(data.sprites.front_default);
+        setIsLoaded(true);
+      })
+      .catch((error) => {
+        console.error("Error fetching Pokemon data:", error);
+        setIsLoaded(false);
+      });
+  };
+
+  const handleInputChange = (event) => {
+    setCurrentValue(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      setSearchValue(currentValue);
+    }
+  };
+
+  const handleSearch = () => {
+    setSearchValue(currentValue);
+  };
 
   return (
     <>
       <h1>Welcome to PokeSearch!!!</h1>
-      <span>Pokemon name: </span><input value={currentValue} onChange={(event) => setCurrentValue(event.target.value)}/>
-      <button onClick={() => setSearchValue(currentValue)}>Search!</button>
-      {isLoaded && <img src={imageURL} />}
+      <span>Pokemon name: </span>
+      <input
+        value={currentValue}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleSearch}>Search!</button>
+      {isLoaded && <img src={imageURL} alt="Pokemon" />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
